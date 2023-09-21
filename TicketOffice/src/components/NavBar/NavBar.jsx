d01c1224e9cd53d84a48ffb7074ecc85207cd896
supergandhi <patrickborjabueno@hotmail.com>
2023-09-21 16:26:34 -0500

    Add component

---
+++ TicketOffice/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div>
+        <p>Events</p>
+        <input placeholder="Find your favorite event"/>
+    </div>
+  );
+}
+
+export default NavBar
